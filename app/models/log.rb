b52ec9b80473eba78f3e82f57b796285c4eadcf2
Tiago Luchini <info@tiagoluchini.eu>
2009-02-14 16:08:22 +0000

    - now log is enoughly recursive (n+1 problem still applies but seems to be the easiest approach at thsi time)

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,52 +1,64 @@
 class Log < ActiveRecord::Base
 
   attr_protected    :trend
+  attr_accessor     :before_log
   
-  belongs_to :user
-
-  validates_presence_of     :user, :date, :weight
+  belongs_to    :user
+  
+  validates_presence_of     :user_id, :date, :weight
+  validates_uniqueness_of   :date, :scope => :user_id
 
   before_save       :calc_trend
 
   def target_weight
-    user, date = self[:user], self[:date]
-    user.target.certain_date(date) if user && user.target && date
+#    user.target.certain_date(date) if user && user.target && date
   end
-
-  def trend=(value)
-    raise NoMethodError, "Can't set trend directly. Calculated field."
-  end  
-
-def get_this
-      user, date, weight, before_log = read_attribute(:user), self[:date], self[:weight], nil
-puts self(:user_id)
-puts @user_id
-puts user
-puts date
-puts weight
-      #begin
-        before_log = Log.find(:first, 
-                        :conditions => ["user_id = :user_id AND date < :date",
-                        {:user_id => user.id, :date => date}])
-      #rescue
-      #end
-end
 
   protected
     def calc_trend
-      user, date, weight, before_log = self[:user], self[:date], self[:weight], nil
-      begin
-        before_log = Log.find(:first, 
-                        :conditions => ["user_id = :user_id AND date < :date",
-                        {:user_id => user.id, :date => date}])
-      rescue
+      process_before_log
+      process_after_log
+    end
+
+    def process_before_log
+      before_log = nil
+      if !@before_log
+        begin
+          before_log = Log.find(:first, 
+                          :conditions => ["user_id = :user_id AND date < :date",
+                          {:user_id => user_id, :date => date}],
+                          :order => 'date desc')
+        rescue
+        end
+      else
+        before_log = @before_log
       end
       if before_log 
         self[:trend] = (weight - before_log.trend)*0.10 + before_log.trend
       else
         self[:trend] = weight
       end
-
     end
     
+    def process_after_log
+      after_log = nil
+      if !@before_log
+        begin
+          after_log = Log.find(:all,
+                        :conditions => ["user_id = :user_id AND date > :date",
+                        {:user_id => user_id, :date => date}],
+                        :order => 'date asc')
+        rescue
+        end
+        if after_log
+          before_log_to_send = self
+          after_log.each do |log|
+            log.trend = nil
+            log.before_log = before_log_to_send
+            log.save
+            before_log_to_send = log
+          end
+        end
+      end
+    end   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -9,20 +9,22 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090213080217) do
+ActiveRecord::Schema.define(:version => 20090214072421) do
 
   create_table "logs", :force => true do |t|
     t.integer  "user_id"
     t.date     "date"
     t.float    "weight"
     t.float    "trend"
-    t.integer  "lock_version", :default => 0
+    t.integer  "lock_version",    :default => 0
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "obs"
+    t.integer  "previous_log_id"
   end
 
   add_index "logs", ["date"], :name => "index_logs_on_date"
+  add_index "logs", ["previous_log_id"], :name => "index_logs_on_previous_log_id"
   add_index "logs", ["user_id"], :name => "index_logs_on_user_id"
 
   create_table "targets", :force => true do |t|
