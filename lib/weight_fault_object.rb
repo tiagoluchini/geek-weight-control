0f066cc95452d9ad945499b445c9813a921115e4
Tiago Luchini <luchini@luchini-laptop.(none)>
2009-04-17 21:22:25 +0300

    Fixed custom fault object to derive from RubyAMF's FaultObject

--- lib/weight_fault_object.rb
+++ lib/weight_fault_object.rb
@@ -1,10 +1,20 @@
-class WeightFaults
+class WeightFaultObject < FaultObject
   
-  cattr_accessor 
-    :UNSPECIFIED,
+  cattr_accessor :UNSPECIFIED,
     :INVALID_LOGIN
   
   @@UNSPECIFIED       = 101
   @@INVALID_LOGIN     = 102
-    
+  
+  @@MSG = {
+    @@UNSPECIFIED => "Unspecified fault!",
+    @@INVALID_LOGIN => "Invalid login!"
+  }
+  
+  def initialize(code) 
+    super(@@MSG[code])
+    self['faultCode'] = code
+    self['code'] = code
+  end
+  
 end
